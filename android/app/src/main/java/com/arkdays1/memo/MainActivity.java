90bc2577f85bbb08162beb218693f67c38531982
arkdays1 <1754394243@qq.com>
2026-09-21 22:46:57 +0800

    chore: add capacitor android project

---
+++ android/app/src/main/java/com/arkdays1/memo/MainActivity.java
@@ -0,0 +1,5 @@
+package com.arkdays1.memo;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
+// 判断是否为Capacitor安卓打包
+const isNativeBuild = process.env.CAPACITOR_BUILD === 'true'
+
 export default defineConfig({
-<<<<<<< HEAD
   base: './',
-=======
->>>>>>> 9b53733c5f5011185310f88b4298cf759c769aae
   plugins: [
     react(),
-    VitePWA({
+    // 只有不是安卓打包的时候，才加载VitePWA
+    ...(!isNativeBuild ? [VitePWA({
       registerType: 'autoUpdate',
       injectRegister: 'auto',
-
       manifest: {
         name: '备忘录',
         short_name: '备忘录',
@@ -20,8 +20,8 @@ export default defineConfig({
         theme_color: '#0b0c10',
         background_color: '#0b0c10',
         display: 'standalone',
-        start_url: '/',
-        scope: '/',
+        start_url: './',
+        scope: './',
         icons: [
           {
             src: '/pwa-192.png',
@@ -35,16 +35,14 @@ export default defineConfig({
           }
         ]
       },
-
       // 让 React SPA 在离线时也能回退到 index.html
       workbox: {
-        navigateFallback: '/'
+        navigateFallback: './index.html'
       },
-
-      // 可选：让你在 dev 模式也能看到 SW 注册（但注意 dev 体验不等于真实安装体验）
+      // 可选：让你在 dev 模式也能看到 SW 注册
       devOptions: {
         enabled: true
       }
-    })
+    })] : [])
   ]
 })
